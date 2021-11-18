bc1da1f67f1c1a9acd3bc72ecbbae703772883c1
jazminejose <jazzi.lyn217@gmail.com>
2021-11-17 17:05:24 -0800

    fix timer

--- script.js
+++ script.js
@@ -1,68 +1,35 @@
 
-//assign variables
-const quizQuestions = [
-  {
-    question: "What does HTML stand for?",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4"
-    },
-    correctAnswer: "b"
-  },
-  {
-    question: "What does javascript do in coding?",
-    answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "What does CSS stand for?"
-  answers: {
-      a: "answer 1",
-      b: "answer 2",
-      c: "answer 3",
-      d: "answer 4"
-    },
-    correctAnswer: "d"
-  }
-]
+// Timer 
+function startTimer() {
+  var counter = 60;
+  setInterval(function () {
+    counter--;
+    if (counter >= 0) {
+      span = document.getElementById("count");
+      span.innerHTML = counter;
+    }
+    if (counter === 0) {
+      // alert('sorry, out of time');
+      // clearInterval(counter);
+      span = document.getElementById("count");
+      span.innerHTML = "00"
+      endQuiz(); 
+    }
+  }, 1000);
+}
+function start() {
+  document.getElementById("count").style = "color:black;";
+  startTimer();
+};
 
-
-//create timer
-i = 60;
-function onTimer() {
-  document.getElementById('timer').innerHTML = i;
-  i--;
-  if (i < 0) {
-    alert('You lose!');
-  }
-  else {
-    setTimeout(onTimer, 1000);
-  }
+function startQuiz() {
+  
 }
 
+function nextQuestion () {
 
+}
 
+function endQuiz () {
 
-
-
-
-
-
-
-
-// document.getElementById('quizOptions').addEventListener('click',)() => newGame {
-// }
-
-// document.getElementById('displayQuestions').addEventListener('click',)() => newGame {
-
-// }
-
-// document.getElementById('scores').style.display ='none'
-
+}
